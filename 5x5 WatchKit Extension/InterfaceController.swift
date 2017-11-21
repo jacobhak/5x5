08c0dcab852b43e4ddf20a33204c603c9f779342
Jacob Håkansson <jacobhakansson@gmail.com>
2017-11-21 16:33:50 +0100

    Initial Commit

---
+++ 5x5 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  5x5 WatchKit Extension
+//
+//  Created by Jacob Håkansson on 2017-11-21.
+//  Copyright © 2017 Jacob Håkansson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
